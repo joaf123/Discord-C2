2da7a66b4d81977b2024eae29de71b046044eb10
Joachim Fosse <56757601+joaf123@users.noreply.github.com>
2024-03-11 20:35:28 +0100

    Refactor Program.cs to make entry points callable from PowerShell

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld;
+
+ public static class Program
+ {
+    //Using add-type this entry point is callable from powershell
+    //[HelloWorld.Program]::Main()
+     public static void Main(string[] args)
+     {
+         Console.WriteLine("Hello, World!");
+     }
+    
+    //Using add-type this entry point is callable from powershell
+    //[HelloWorld.Program]::TestMessage()
+     public static string TestMessage() {
+         return "Hello, World!";
+     }
+ }
