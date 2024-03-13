9486991e7a202611c6b5c2dc1523e43d42b4ac27
Joachim Fosse <fojo1709@gmail.com>
2024-03-13 19:23:30 +0100

    Working proof of concept

--- Program.cs
+++ Program.cs
@@ -1,17 +1,46 @@
-﻿namespace HelloWorld;
+﻿using DSharpPlus;
 
- public static class Program
- {
-    //Using add-type this entry point is callable from powershell
-    //[HelloWorld.Program]::Main()
-     public static void Main(string[] args)
+namespace DiscordC2;
+
+ public class Server {
+     public static async Task Main(string[] args)
      {
-         Console.WriteLine("Hello, World!");
+         var discord = new DiscordClient(new DiscordConfiguration() {
+            Token = File.ReadAllText(".dc2"),
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+        });
+        
+        discord.MessageCreated += async (s, e) => {
+            if (e.Message.Content.ToLower().StartsWith("ping")) {
+                await e.Message.RespondAsync("pong!");
+            }
+        };
+        Console.WriteLine("DiscordC2::Server booting..");
+        await discord.ConnectAsync();
+        Console.WriteLine("DiscordC2::Booting finished..");
+        await Task.Delay(-1);
+        Console.WriteLine("DiscordC2::Server task exited unexpectedly..");
      }
-    
     //Using add-type this entry point is callable from powershell
-    //[HelloWorld.Program]::TestMessage()
-     public static string TestMessage() {
-         return "Hello, World!";
+    //[DiscordC2.Server]::Start()
+     public static async Task Start() {
+        var discord = new DiscordClient(new DiscordConfiguration() {
+            Token =  File.ReadAllText("/.dc2"),
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+        });
+
+        discord.MessageCreated += async (s, e) => {
+            if (e.Message.Content.ToLower().StartsWith("ping")) {
+                await e.Message.RespondAsync("pong!");
+            }
+        };
+
+        Console.WriteLine("DiscordC2::Server booting..");
+        await discord.ConnectAsync();
+        Console.WriteLine("DiscordC2::Booting finished..");
+        await Task.Delay(-1);
+        Console.WriteLine("DiscordC2::Server task exited unexpectedly..");
      }
  }
